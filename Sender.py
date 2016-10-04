859ea92c9f16e34c77897efb28a95b0f0f6e70bf
pmop <pmop@ic.ufal.br>
2016-10-03 23:45:30 -0300

    Sender depends on paramiko and scp to send files over ssh. Client's public key MUST BE KNOWN by host. We will have to add : 	Internet connection check; 	Error checking; 	Errors at sending files; 	Status logging;

---
+++ Sender.py
@@ -0,0 +1,30 @@
+import paramiko
+from paramiko import SSHClient
+from scp import SCPClient
+
+class SendData(object):
+    _sshclient = None
+    _scpclient = None
+    #Example:
+    #sender = SendData ('11.22.33.44','username')
+    def __init__(self,serverip,rusername):
+        self._sshclient = SSHClient()
+        self._sshclient.set_missing_host_key_policy (paramiko.AutoAddPolicy())
+        # Server needs to have your key
+        self._sshclient.load_system_host_keys()
+        self._sshclient.connect (hostname=serverip,username=rusername)
+        self._scpclient = SCPClient (self._sshclient.get_transport ())
+        #Example: send (pathtofile)
+    def send (self,file):
+        self._scpclient.put (file)
+
+    def end (self):
+        self._scpclient.close()
+
+def main ():
+    sender = SendData ('159.203.103.181','aeolus')
+    sender.send ('/home/pedro/TESTFILE')
+    sender.end()
+
+if __name__ == "__main__":
+    main()
