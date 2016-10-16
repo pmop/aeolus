9251ded4ca434f64e70e096323107b7727dde13f
pmop <pmop@ic.ufal.br>
2016-10-16 12:21:35 -0300

    Added class for daemonizing classes and daemonized Sender daemon.py : Services now inherits daemon so they can run as a UNIX daemon Sender.py : Sender now lookups ~/SensorData and sends everything that is here, Sender also will log its activity at ~/SENSORLOG.

--- Sender.py
+++ Sender.py
@@ -1,6 +1,11 @@
 import paramiko
 from paramiko import SSHClient
 from scp import SCPClient
+from daemon import Daemon
+from os.path import exists, expanduser, isfile, join
+from os import listdir, remove
+import datetime
+import time, sys
 
 class SendData(object):
     _sshclient = None
@@ -15,16 +20,94 @@ class SendData(object):
         self._sshclient.connect (hostname=serverip,username=rusername)
         self._scpclient = SCPClient (self._sshclient.get_transport ())
         #Example: send (pathtofile)
-    def send (self,file):
-        self._scpclient.put (file)
+    def send (self,file,remotepath='.'):
+        success = False
+        status = "01 Didn't do anything"
+        try:
+            transport = self._sshclient.get_transport()
+            transport.send_ignore()
+            self._scpclient.put (file,remote_path=remotepath)
+            success = True
+            status = "00 OK"
+        except EOFError, e:
+            success = False
+            status = "02 No connection"
+        return success,status
 
     def end (self):
         self._scpclient.close()
 
-def main ():
-    sender = SendData ('159.203.103.181','aeolus')
-    sender.send ('/home/pi/TESTFILE')
-    sender.end()
 
+## Sender daemon
+class Sender(Daemon):
+#class Sender(object):
+    def run(self):
+        log = []
+        con = SendData ('159.203.103.181','aeolus')
+        while (True):
+            sent = []
+            toSend = self.__listData()
+
+            for file in toSend:
+                success,status = con.send (file,"~/SensorData")
+                if success:
+                    sent.append (file)
+                    log.append (self.__timeNow() +  " " +
+                                       status + " " + file)
+                else:
+                    mesg = "Couldn't send " + status + " "
+                    log.append (self.__timeNow() + mesg  + file)
+                    con = SendData ('159.203.103.181','aeolus')
+
+            self.__wipeSentData (sent)
+            self.__writeToLog (log)
+            time.sleep (1)
+            if len(log) > 0:
+                log[:] = []
+
+    def __listData(self):
+        print "listing files"
+        dir =  expanduser("~") + "/SensorData"
+        files = None
+        if exists(dir):
+            files = [join(dir,f) for f in listdir(dir) if isfile(join(dir,f)) ]
+        return files
+
+    def __timeNow(self):
+        return unicode(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    def __wipeSentData(self,fileList):
+        print "wiping files"
+        for file in fileList:
+            if isfile(file):
+                remove(file)
+
+    def __writeToLog(self,log):
+        print "logging proc"
+        dir = expanduser ("~")
+        file = open (join (dir,"SENDERLOG"),"a")
+        for str in log:
+            file.write (str+"\n")
+        file.close ()
+
+def main ():
+    daemon = Sender('/tmp/dsender.pid')
+ #   dsender = Sender()
+  #  dsender.run ()
+  #  dsender.stop ()
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
 if __name__ == "__main__":
     main()
