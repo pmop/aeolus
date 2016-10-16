2b475ddff22fcac45d255e3c616890971849dbd7
pmop <pmop@ic.ufal.br>
2016-10-16 15:25:08 -0300

    Started DataMonitor daemonization and fixed Sender behaviour when there is no connection

--- DataMonitor.py
+++ DataMonitor.py
@@ -5,73 +5,72 @@ import RawObject
 import json
 import datetime
 import os
+from daemon import Daemon
+from RawObject import RawObject
+
 
 #Saves data as filename at path, checking if path exists and
 #creating it if doesn't
-def saveData (filename,path,data):
-        if os.path.isdir (path):
-                file = open (path +"/" + filename,"w")
-                file.write (data)
-                file.close
-        else:
-                os.mkdir (path)
-                saveData (filename,
-                          path,data)
-
 
 ## Setting up things
-GPIO.setmode(GPIO.BOARD)
-rainSensorPine = 11
-tempSensor = Adafruit_DHT.DHT11
-tempSensorPine = 22
-refresh = 60
-GPIO.setup(11, GPIO.IN)
-
-pathSaveData = os.path.expanduser ("~") +"/SensorData"
 
 print ("Aeolus Data Monitor\n")
 print ("Reading values from Sensors")
 
-while (1):
-	hum, temp = Adafruit_DHT.read_retry(tempSensor, tempSensorPine)
-	## When GPIO 11 goes to 0, it detected a rain drop
-	rain = not GPIO.input(rainSensorPine)
+class DataMon (Daemon):
+        def run(self):
 
-	if hum is not None and temp is not None:
-		## Show status on console
-		print ("Temp = {0:0.1f} and Hum = {1:0.1f}\n").format(temp, hum)
-		if rain:
-			print ("Raining")
+                GPIO.setmode(GPIO.BOARD)
+                rainSensorPine = 11
+                tempSensor = Adafruit_DHT.DHT11
+                tempSensorPine = 22
+                refresh = 60
+                GPIO.setup(11, GPIO.IN)
 
-	        ## Create RawData
-    		rawDHT11 = {
-        		"temp" : temp,
-         		"hum" : hum
-    		}
+                pathSaveData = os.path.expanduser ("~") +"/SensorData"
+                while (True):
+                        hum, temp = Adafruit_DHT.read_retry(tempSensor,
+                                                            tempSensorPine)
+                        #Get time closest as possible to sensor read call
+                        currentdate = unicode(datetime.
+                                              datetime.now()
+            		                          .strftime("%Y-%m-%d %H:%M:%S"))
+                        rain = not GPIO.input(rainSensorPine)
+                        if hum is not None and temp is not None:
+                                rawDHT11 = {
+                                        "temp": temp,
+                                        "hum" : hum
+                                }
+                                rawYL83 = {
+        		                        "rain" : rain
+	                            }
+                                dht11Object = RawObject (rawDHT11,
+                                                         "DHT11",
+                                                         currentdate)
+                                yl83Object = RawObject (rawYL83,
+                                                        "YL83",
+                                                        currentdate)
 
-	        rawYL83 = {
-        		"rain" : rain
-	        }
+                                self.__saveData
+                                (dht11Object.getDate() + ".json",
+                                          pathSaveData,
+                                          dht11Object.getJsonData() )
 
-    		## RawObject gets created
-	    	dht11Object = RawObject.RawObject (rawDHT11,"DHT11",unicode(
-        	    	datetime.datetime.now()
-            		.strftime("%Y-%m-%d %H:%M:%S")))
+                                self.__saveData
+                                (yl83Object.getDate() + "C.json",
+                                          pathSaveData,
+                                          yl83Object.getJsonData() )
+                        else:
+                                print ("Couldn't retrieve information")
+                time.sleep (6) # sleeps for 6 seconds
 
-	        yl83Object = RawObject.RawObject (rawYL83, "YL83", unicode(
-        	        datetime.datetime.now()
-                	.strftime("%Y-%m-%d %H:%M:%S")))
+        def __saveData (self,filename,path,data):
+                if os.path.isdir (path):
+                        file = open (path +"/" + filename,"w")
+                        file.write (data)
+                        file.close
+                else:
+                        os.mkdir (path)
+                        saveData (filename,
+                                  path,data)
 
-	    	## Data gets saved at ~/SensorData
-    		saveData (dht11Object.getDate() + ".json",
-            		pathSaveData, dht11Object.getJsonData() )
-
-	        saveData (yl83Object.getDate() + "C.json",
-          		pathSaveData, yl83Object.getJsonData() )
-
-   		## Sender will be launched at another thread.
-
-	else:
-		print ("Temp/Hum Retrieve Failed")
-
-	time.sleep(refresh)
--- Sender.py
+++ Sender.py
@@ -6,6 +6,7 @@ from os.path import exists, expanduser, isfile, join
 from os import listdir, remove
 import datetime
 import time, sys
+import requests
 
 class SendData(object):
     _sshclient = None
@@ -43,13 +44,20 @@ class Sender(Daemon):
 #class Sender(object):
     def run(self):
         log = []
-        con = SendData ('159.203.103.181','aeolus')
+        isCon = self.__checkConnection()
+        con = None
         while (True):
             sent = []
             toSend = self.__listData()
+            # Inits ssh if has connection
+            if isCon and con == None:
+                con = SendData ('159.203.103.181','aeolus')
 
             for file in toSend:
-                success,status = con.send (file,"~/SensorData")
+                success = False
+                status = "No connection"
+                if isCon and con != None:
+                    success,status = con.send (file,"~/SensorData")
                 if success:
                     sent.append (file)
                     log.append (self.__timeNow() +  " " +
@@ -57,13 +65,22 @@ class Sender(Daemon):
                 else:
                     mesg = "Couldn't send " + status + " "
                     log.append (self.__timeNow() + mesg  + file)
-                    con = SendData ('159.203.103.181','aeolus')
+                    isCon = self.__checkConnection()
+                    if isCon:
+                        con = SendData ('159.203.103.181','aeolus')
 
             self.__wipeSentData (sent)
             self.__writeToLog (log)
             time.sleep (1)
             if len(log) > 0:
                 log[:] = []
+
+    def __checkConnection (self,url='http://www.google.com',timeout=5):
+        try:
+            _ = requests.get (url,timeout=timeout)
+            return True
+        except requests.ConnectionError:
+            return False
 
     def __listData(self):
         print "listing files"
@@ -92,8 +109,8 @@ class Sender(Daemon):
 
 def main ():
     daemon = Sender('/tmp/dsender.pid')
- #   dsender = Sender()
-  #  dsender.run ()
+  #  dsender = Sender()
+ #   dsender.run ()
   #  dsender.stop ()
     if len(sys.argv) == 2:
         if 'start' == sys.argv[1]:
