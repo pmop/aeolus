f230027fbe76cff6efd36da7358327039a8bf7d7
glauberrleite <grl@ic.ufal.br>
2016-10-16 12:19:24 +0000

    DataMonitor working flawlessly
    Added the saveData method to DataMonitor

--- DataMonitor.py
+++ DataMonitor.py
@@ -14,7 +14,7 @@ def saveData (filename,path,data):
                 file.write (data)
                 file.close
         else:
-                os.mkdir (pathToData)
+                os.mkdir (path)
                 saveData (filename,
                           path,data)
 
@@ -23,7 +23,7 @@ GPIO.setmode(GPIO.BOARD)
 rainSensorPine = 11
 tempSensor = Adafruit_DHT.DHT11
 tempSensorPine = 22
-refresh = 1
+refresh = 60
 
 pathSaveData = os.path.expanduser ("~") +"/SensorData"
 
@@ -33,31 +33,25 @@ print ("Reading values from Sensors")
 while (1):
 	hum, temp = Adafruit_DHT.read_retry(tempSensor, tempSensorPine)
     ## When GPIO 11 goes to 0, it detected a rain drop
-    rain = not GPIO.input(rainSensorPine)
+    ##rain = not GPIO.input(rainSensorPine)
 
 	if hum is not None and temp is not None:
-
-
-
-        print dataobject.getJsonData()
-
 		print ("Temp = {0:0.1f} and Hum = {1:0.1f}\n").format(temp, hum);
-
-        ## Create RawObject
-        rawdata = {
-                "temp" : temp,
-                "hum" : hum,
-        }
-        ## RawObject gets created
-        dataobject = RawObject.RawObject (rawdata,"DHT11",unicode(
-                datetime.datetime.now()
-                .strftime("%Y-%m-%d %H:%M:%S")))
-        ## Data gets saved at ~/SensorData
-        saveData (dataobject.getDate() + ".json",
-                  pathSaveData, dataobject.getJsonData() )
-        ## Sender will be launched at another thread. Will
+        	## Create RawObject
+        	rawdata = {
+                	"temp" : temp,
+                	"hum" : hum,
+        	}
+        	## RawObject gets created
+        	dataobject = RawObject.RawObject (rawdata,"DHT11",unicode(
+                	datetime.datetime.now()
+                	.strftime("%Y-%m-%d %H:%M:%S")))
+        	## Data gets saved at ~/SensorData
+        	saveData (dataobject.getDate() + ".json",
+                	  pathSaveData, dataobject.getJsonData() )
+       		 ## Sender will be launched at another thread. Will
 
 	else:
 		print ("Temp/Hum Retrieve Failed")
 
-	time.sleep(60)
+	time.sleep(refresh)
