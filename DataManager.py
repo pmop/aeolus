cc41c481488a6e8623ceb748c050021d48cf9211
pmop <pmop@ic.ufal.br>
2016-10-16 22:38:14 -0300

    Implementing DataManager: 	DataManager generates endpoints for data collect. First, we need 	to pass which keys we will be reading from rawData, we call 	these keys yAxis. Then DataManager will generate a endpoint for 	each one if it doesn't exists already, at ~/endpoints, each 	named with it's key name. Then DataManager will append to y: [] 	values from each according key and date from each to x:[]

--- DataManager.py
+++ DataManager.py
@@ -1,48 +1,105 @@
 from RawObject import RawObject
 from daemon import Daemon
+from os.path import expanduser, exists, isfile, join
+import json
 
-class DataManager (Daemon):
-    """Class DataManager
-    Picks up data at ~/SensorData and turns it into final json endpoint
-    """
-    # Attributes:
+class DataManager (object):
+    __pathToData = None
+    __xAxisKeys = []
+    __yAxisKeys = []
+    #def __init__(self,pidfile):
+       #Daemon.__init__(self,pidfile=pidfile)
+    def __init__(self):
+        self.__pathToData = expanduser ("~") + "/SensorData"
 
-    # Operations
-    def storeData(self, object):
-        """function storeData
+    def __listFiles(self):
+        print "listing files"
+        dir =  self.__pathToData
+        files = None
+        if exists(dir):
+            files = [join(dir,f)
+                     for f in listdir(dir) if isfile(join(dir,f)) ]
+        return files
 
-        object: RawObject
+    def setDataPath(self,path):
+        self.__pathToData (path)
 
-        returns void
+    def setXAxisKeys (self,keylists):
+        self.__xAxisKeys = keylists
+
+    def appendXAxisKey (self,key):
+        self.__xAxisKeys.append (key)
+
+    def setYAxisKeys (self,keylists):
+        self.__yAxisKeys = keylists
+
+    def appendYAxisKey (self,key):
+        self.__yAxisKeys.append (key)
+    # Either generates or appends to endpoint
+    def generateBaseEndpoints (self):
+
+        #fileList = self.__listFiles()
+        dir = expanduser ("~") + "/endpoints"
+        # Dict of keys -> files that will be generated
+        basefiles = dict()
+        for key in self.__yAxisKeys:
+            if not exists (join (dir,key) ):
+                basefiles[key] = join(dir,key)
+        print basefiles
+        if len (basefiles) > 0:
+            endpoints = dict()
+            for yAxisKey in self.__yAxisKeys:
+                print yAxisKey
+                f = open (basefiles[yAxisKey],"w")
+                endpoints[yAxisKey] = {
+                    "data": [
+                        {
+                            "type": "scatter",
+                            "mode": "lines",
+                            "x": [
+                            ],
+                            "y": [
+                            ],
+                            "line": {
+                                "width": 1
+                            },
+                            "error_y": {
+                                "array": [
+                                ],
+                                "thickness": 0.5,
+                                "width": 0
+                            }
+                        }
+                    ],
+                    "layout": {
+                        "yaxis": {
+                            "title": yAxisKey
+                        },
+                        "xaxis": {
+                            "showgrid": "false",
+                            "tickformat": "%B, %Y"
+                        },
+                        "margin": {
+                            "l": 40,
+                            "b": 10,
+                            "r": 10,
+                            "t": 20
+                        }
+                    }
+                }
+                f.write (json.dumps (endpoints[yAxisKey]))
+                f.close ()
+            
         """
-        return None # should raise NotImplementedError()
-
-    def getData(self):
-        """function getData
-
-        returns RawObject
+        for file in fileList:
+            with open(fileList) as data_file:
+                data = json.load (data_file)
         """
-        return None # should raise NotImplementedError()
-
-    def __connect(self):
-        """function connect
-
-        returns void
-        """
-        return None # should raise NotImplementedError()
-
-    def __disconnect(self):
-        """function disconnect
-
-        returns
-        """
-        return None # should raise NotImplementedError()
-
-    def __validate(self):
-        """function validate
-
-        returns
-        """
-        return None # should raise NotImplementedError()
+def main():
+    test = DataManager ()
+    test.setYAxisKeys (["hum","temp"])
+    test.generateBaseEndpoints()
 
 
+if __name__ == "__main__":
+    main()
